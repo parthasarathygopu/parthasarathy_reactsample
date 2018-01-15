6a030deb1fc6cb7fe5e0e81c09459e3eda8d621b
Parthasarathy G <parthasarathy_g@photonint.com>
2018-01-15 10:32:32 +0530

    deteled

--- cafeteria_full_project/dev/js/reducers/reducer-Login.js
+++
@@ -1,8 +0,0 @@
-export default function (state = null, action) {
-    switch (action.type) {
-        case "USER_LOGIN":
-            return [action.payload];
-        default:
-            return state;
-    }
-}
