b13c1125199d3843837924e722ad6c89b21b0af8
Parthasarathy G <parthasarathy_g@photonint.com>
2018-01-15 10:31:27 +0530

    deteled

--- cafeteria_project/dev/js/reducers/activeItemReducer.js
+++
@@ -1,9 +0,0 @@
-export default function (state = null, action) {
-    switch (action.type) {
-        case 'ITEM_SELECTED':
-            return action.payload;
-            break;
-    }
-    return state;
-}
-
--- cafeteria_project/dev/js/reducers/reducer-final-order.js
+++
@@ -1,9 +0,0 @@
-const data = [];
-export default function (state = data, action) {
-    switch (action.type) {
-        case 'ADD_FINAL_CART':
-            return action.payload;
-            break;
-    }
-    return state;
-}
